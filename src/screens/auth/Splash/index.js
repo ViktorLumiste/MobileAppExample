a88f3c53dec3a82069b37d9be9174124b93234b7
Viktor Lumiste <viktor.lumiste@gmail.com>
2024-02-08 10:02:06 +0200

    Made an error :)

---
+++ src/screens/auth/Splash/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import {Text} from "react-native"
+
+const Splash = () => {
+    return(
+        <Text>You'll find all you need here!</Text>
+    )
+}
+export default Splash
