b8ec9ebb0bb0235e50a43838d7ab1532e625b1a0
Viktor Lumiste <viktor.lumiste@gmail.com>
2024-02-09 13:04:32 +0200

    Checkbox komponent

--- src/screens/auth/SignUp/styles.js
+++ src/screens/auth/SignUp/styles.js
@@ -5,5 +5,18 @@ import {colors} from "../../../utils/colors"
 export const styles = StyleSheet.create({
     container:{
         padding:24
+    },
+    agreeRow:{
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    agreeText:{
+        color: colors.blue,
+        marginHorizontal: 14
+    },
+    agreeTextBold:{
+        color: colors.blue,
+        marginHorizontal: 14,
+        fontWeight:'bold'
     }
 })
