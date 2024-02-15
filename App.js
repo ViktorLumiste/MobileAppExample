a9de79b28c0b028cc81ec5263da01af0bafb2b62
Viktor Lumiste <viktor.lumiste@gmail.com>
2024-02-15 14:12:46 +0200

    wip

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React, {useEffect} from 'react';
 import { SafeAreaView } from "react-native";
-import SignIn from './src/screens/auth/SignIn';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SplashScreen from './src/screens/auth/Splash';
 
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
 
 import Config from "react-native-config"
+import Splash from './src/screens/auth/Splash';
 
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   useEffect(() => {
@@ -18,7 +22,11 @@ const App = () => {
   },[])
   return(
     <SafeAreaView>
-      <SignIn />
+      <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen name="Splash" component={Splash} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </SafeAreaView>
   );
 };
