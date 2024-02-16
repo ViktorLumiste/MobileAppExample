69659fa83fb67c2daf513e9f20d973970371108a
Viktor Lumiste <viktor.lumiste@gmail.com>
2024-02-16 10:44:24 +0200

    Create navigation between screens and tabs

--- App.js
+++ App.js
@@ -1,29 +1,68 @@
 import React, {useEffect} from 'react';
-import { colors } from './src/utils/colors';
+
+import Splash from './src/screens/auth/Splash';
+import SignUp from './src/screens/auth/SignUp';
+import SignIn from './src/screens/auth/SignIn';
+
+import Home from './src/screens/app/Home';
+import Favorites from './src/screens/app/Favorites';
+import Profile from './src/screens/app/Profile';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
 
+import {Image} from "react-native"
 import Config from "react-native-config"
-import Splash from './src/screens/auth/Splash';
-import SignUp from './src/screens/auth/SignUp';
-import SignIn from './src/screens/auth/SignIn';
+import { colors } from './src/utils/colors';
 
-const theme = {
-  colors:{
-    background: colors.white
-  }
-}
+
 
 const Stack = createNativeStackNavigator();
 
+const Tab = createBottomTabNavigator();
 
+const Tabs = () => {
+  return(
+    <Tab.Navigator 
+    screenOptions={({ route }) =>({
+      tabBarIcon: ({ focused, color, size }) => {
+        let icon;
+
+        if (route.name === 'Home' ){
+          icon = focused
+            ? require('./src/assets/tabs/home_active.png')
+            : require('./src/assets/tabs/home.png');
+        } else if ( route.name === 'Favorites' ){
+          icon = focused
+            ? require('./src/assets/tabs/bookmark_active.png')
+            : require('./src/assets/tabs/bookmark.png');
+        } else if ( route.name === 'Profile'){
+          icon = focused
+            ? require('./src/assets/tabs/profile_active.png')
+            : require('./src/assets/tabs/profile.png');
+        }
+        return <Image style={{width:24, height:24}} source={icon} />;
+    },
+    headerShown: false,
+    tabBarShowLabel: false,
+    tabBarStyle: {borderTopColor:colors.lightGray}
+    })}
+    >
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Favorites" component={Favorites} />
+      <Tab.Screen name="Profile" component={Profile} />
+    </Tab.Navigator>
+  );
+}
 
 const App = () => {
+  const isSignedIn = true
+
   useEffect(() => {
     GoogleSignin. configure({
       scopes: ['https://www.googleapis.com/auth/drive.readonly'],
@@ -31,14 +70,30 @@ const App = () => {
       offlineAccess: true,
       iosClientId: Config.GOOGLE_IOS_CLIENT_ID,
       })
-  },[])
+  },[]);
+
+  const theme = {
+    colors:{
+      background: colors.white
+    }
+  };
+
   return(
     <SafeAreaProvider>
     <NavigationContainer theme={theme}>
       <Stack.Navigator>
+        {
+          isSignedIn?(
+            <>
+              <Stack.Screen name="Tabs" component={Tabs} options={{headerShown:false}} />
+            </>
+          ) : (
+            <>
           <Stack.Screen name="Splash" component={Splash} options={{headerShown: false}} />
           <Stack.Screen name="SignUp" component={SignUp} options={{headerShown: false}} />
           <Stack.Screen name="SignIn" component={SignIn} options={{headerShown: false}} />
+          </>
+          )}
       </Stack.Navigator>
     </NavigationContainer>
     </SafeAreaProvider>
---
+++ src/screens/app/Home/styles.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+import {colors} from "../../../utils/colors"
+
+export const styles = StyleSheet.create({
+        container: {
+        
+        },
+        image:{
+            
+        },
+        titleContainer:{
+            
+        },
+        title:{
+            
+        },
+        innerTitle:{
+            
+        },
+        footerText: {
+            
+        }
+})
