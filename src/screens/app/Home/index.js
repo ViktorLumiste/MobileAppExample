4881ed3bdad09153ce5d954812491c649164cbaf
Viktor Lumiste <viktor.lumiste@gmail.com>
2024-02-16 11:08:08 +0200

    Header

--- src/screens/app/Home/index.js
+++ src/screens/app/Home/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { View, Text } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context";
+import Header from "../../../components/Header";
 import { styles } from "./styles"
 
 const Home = () => {
     return(
         <SafeAreaView>
             <View style={styles.container}>
+                <Header showSearch={true} title="Find All You Need"></Header>
                 <Text>Home</Text>
             </View>
         </SafeAreaView>
